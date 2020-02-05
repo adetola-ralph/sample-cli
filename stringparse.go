37f39c43f279d2a239ae8a0e4bed4204c5589a4c
Ralph Olutola <oreofe.olutola@gmail.com>
2020-02-05 23:52:53 +0100

    Add cli flags and default values

--- stringparse.go
+++ stringparse.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	textPtr := flag.String("text", "", "Text to parse")
+	metricPtr := flag.String("metric", "char", "Metric {chars|words:lines};.")
+	uniquePtr := flag.Bool("unique", false, "Measure unique value of a mteric")
+	flag.Parse()
+
+	fmt.Printf("textPtr: %s, metricPtr: %s, uniquePtr: %t\n", *textPtr, *metricPtr, *uniquePtr)
+}
