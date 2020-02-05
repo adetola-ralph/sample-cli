5bffadeca527f57e0f72965b19fa2dd1dc47db0a
Ralph Olutola <oreofe.olutola@gmail.com>
2020-02-06 00:11:52 +0100

    Make text & metric required

--- stringparse.go
+++ stringparse.go
@@ -3,13 +3,38 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	textPtr := flag.String("text", "", "Text to parse")
-	metricPtr := flag.String("metric", "char", "Metric {chars|words:lines};.")
+	metricChoices := [3]string{"chars", "words", "lines"}
+
+	textPtr := flag.String("text", "", "Text to parse (Required)")
+	metricPtr := flag.String("metric", "char", "Metric {chars|words:lines};. (Required)")
 	uniquePtr := flag.Bool("unique", false, "Measure unique value of a mteric")
 	flag.Parse()
 
+	if *textPtr == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if exists, _ := includes(metricChoices[:], *metricPtr); !exists {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	fmt.Println(textPtr)
+
 	fmt.Printf("textPtr: %s, metricPtr: %s, uniquePtr: %t\n", *textPtr, *metricPtr, *uniquePtr)
+}
+
+func includes(array []string, value string) (bool, int) {
+	for i, v := range array {
+		if value == v {
+			return true, i
+		}
+	}
+
+	return false, -1
 }
